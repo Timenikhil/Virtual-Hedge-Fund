a44f4cfdefdb0a807bbb77036204b7c36f068a1f
Timenikhil <48746822+Timenikhil@users.noreply.github.com>
2025-10-29 16:36:41 +0800

    clean up code

--- vhf/logging/log.py
+++ vhf/logging/log.py
@@ -13,16 +13,10 @@ LOKI_USERNAME = os.getenv("LOKI_USERNAME")
 LOKI_PASSWORD = os.getenv("LOKI_PASSWORD")
 
 
-# 1. Get the root logger
-# You can also use a specific logger like: logger = logging.getLogger("my_app")
 logger = logging.getLogger("vhf")
 logger.setLevel(logging.INFO) # Set the lowest level of logs to be handled
 
-# 2. Create a formatter to define the log message format
-# This format will be used by all handlers unless overridden
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# 3. Create a handler to write logs to a local file
 # TimedRotatingFileHandler will rotate the log file daily, keeping the last 7 files
 file_handler = TimedRotatingFileHandler(
     filename=LOG_FILE_PATH,
@@ -32,16 +26,13 @@ file_handler = TimedRotatingFileHandler(
 )
 file_handler.setFormatter(formatter)
 
-# 4. Create a handler to send logs to Grafana Loki
 loki_handler = LokiQueueHandler(
     url=LOKI_URL,
     auth=(LOKI_USERNAME, LOKI_PASSWORD),
     tags={"application": "virtual-hedge-fundF", "environment": "production"},
     version="1",
-
 )
 
-# 5. Add BOTH handlers to the logger
 logger.addHandler(file_handler)
 logger.addHandler(loki_handler)
 
