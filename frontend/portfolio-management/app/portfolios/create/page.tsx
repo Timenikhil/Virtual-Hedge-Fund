2a168e0428e4dcd6fdeb7e17558fe0e86a163363
Timenikhil <48746822+Timenikhil@users.noreply.github.com>
2026-01-26 14:29:17 +0800

    frontend weight improve

--- frontend/portfolio-management/app/portfolios/create/page.tsx
+++ frontend/portfolio-management/app/portfolios/create/page.tsx
@@ -326,7 +326,7 @@ export default function PortfolioManagement() {
 
                             <button
                                 onClick={handleStep4Submit}
-                                disabled={!portfolioId}
+                                disabled={!portfolioId || (selectedStrategies.length != weights.length && workflowMode === 'manual')}
                                 className="w-full px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 disabled:bg-gray-400 disabled:cursor-not-allowed"
                             >
                                 Set Weights
